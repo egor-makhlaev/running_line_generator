1f2903d8c77880bf65de159fdda31c6f30750ba8
Egor Makhlaev <68485879+egor-makhlaev@users.noreply.github.com>
2023-06-27 20:20:58 +0300

    Update utils.py

--- runtext/utils.py
+++ runtext/utils.py
@@ -4,11 +4,16 @@ from PIL import Image,ImageDraw,ImageFont
 
 
 def create_scrolling_text_video(text, output_name):
-    font = ImageFont.truetype('/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf', 32)
-    image_width=100 # Width must be 100 according to the technical requirements
-    image_height=100 # Height must be 100 according to the technical requirements
-    text_color=(255, 255, 255) # Scrolling text color is white as in the example video
-    bg_color=(255, 0, 255) # Background color is magenta as in the example video
+    font_path = Path(__file__).resolve().parent.parent / 'static/arial.ttf'
+    font = ImageFont.truetype(font_path, 32)
+    # Width must be 100 according to the technical requirements
+    image_width=100
+    # Height must be 100 according to the technical requirements
+    image_height=100
+    # Scrolling text color is white as in the example video
+    text_color=(255, 255, 255)
+    # Background color is magenta as in the example video
+    bg_color=(255, 0, 255)
     # Get size of the input text string
     text_width, text_height = font.getsize(text)
     # Number of frames
