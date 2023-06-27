42ab1106586fb92068a52562ce359fe786d72e97
Egor Makhlaev <egor.makhlaev@gmail.com>
2023-06-27 15:58:11 +0300

    first commit

---
+++ runtext/utils.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+from PIL import Image,ImageDraw,ImageFont
+
+
+def create_scrolling_text_video(text, output_name):
+    font = ImageFont.truetype('arial.ttf', 32)
+    image_width=100 # Width must be 100 according to the technical requirements
+    image_height=100 # Height must be 100 according to the technical requirements
+    text_color=(255, 255, 255) # Scrolling text color is white as in the example video
+    bg_color=(255, 0, 255) # Background color is magenta as in the example video
+    # Get size of the input text string
+    text_width, text_height = font.getsize(text)
+    # Number of frames
+    num_frames = text_width + image_width
+    # Calculate FPS, knowing that duration must be 3 seconds
+    fps = num_frames / 3
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter(
+        output_name,
+        fourcc,
+        fps,
+        (image_width, image_height)
+    )
+    # Create frames for text scrolling
+    for i in range(num_frames):
+        img = Image.new('RGB', (image_width, image_height), bg_color)
+        draw = ImageDraw.Draw(img)
+        x = image_width - i
+        y = (image_height - text_height) // 2
+
+        draw.text((x, y), text, font=font, fill=text_color)
+        # Convert the image to an OpenCV frame
+        frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+        video_writer.write(frame)
+
+    video_writer.release()
+ 
---
+++ runtext/views.py
@@ -0,0 +1,23 @@
+import os
+from runtext.models import Message
+from django.shortcuts import render
+from wsgiref.util import FileWrapper
+from django.http import HttpResponse
+from runtext.utils import create_scrolling_text_video
+
+OUTPUT_NAME = 'media/my_video.mp4'
+
+
+def index(request):
+    text = request.GET['text']
+
+    Message.objects.create(msg=text)
+
+    create_scrolling_text_video(text, OUTPUT_NAME)
+    file = FileWrapper(open(OUTPUT_NAME, 'rb'))
+    response = HttpResponse(file, content_type='video/mp4')
+    response['Content-Disposition'] = 'attachment; filename=my_video.mp4'
+
+    os.remove(OUTPUT_NAME)
+
+    return response
